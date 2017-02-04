4ffc6ca3f2bd3d28fdb5e731a97ed82b806b3c50
dedoff.daniil@yandex.ru <H23n&@jsd_1VaGr>
2017-02-04 14:52:07 +1000

    Refs

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,25 @@
 var React= require('react');
 
+var{Link}=require('react-router');
+
 var Examples = (props)=>{
   return(
     <div>
-  <h3>Examples Changed Component!</h3>
-  <p>Welcome tp page</p>
+  <h1 className="text-center">Examples</h1>
+  <p>Few locations</p>
+  <ol>
+    <li>
+      <Link to='/?location=London'>London</Link>
+    </li>
+
+    <li>
+        <Link to='/?location=Moscow'>Moscow</Link>
+
+
+    </li>
+
+
+  </ol>
   </div>
   );
 };
