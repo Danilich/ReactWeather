da0c7819f2e068ad9573d2442d8f930d80a6f7a1
dedoff.daniil@yandex.ru <H23n&@jsd_1VaGr>
2017-02-04 15:00:04 +1000

    About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,22 @@ var React= require('react');
 var About = (props)=>{
   return(
       <div>
-  <h3>About Component</h3>
-    <p>Welcome About page</p>
+  <h1 className="text-center">About Component</h1>
+    <p>This React app </p>
+    <p>
+
+      Here some of the tools
+    </p>
+    <ul>
+      <li>
+      <a href="https://facebook.github.io/react">React</a> -This was
+        the Javascript framework
+      </li>
+      <li>
+        <a href="https://openweathermap.org">Open Weather Map</a>- I used
+          Open weather map to search for weather by city name.
+      </li>
+    </ul>
     </div>
   );
 };
